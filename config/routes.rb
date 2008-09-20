356bd86826d2f685ac7a4b1754217c408a329d3b
Chris Nolan.ca <chrisnolan.ca+github@gmail.com>
2008-09-19 20:19:27 -0400

    Switch comments to make_resourceful

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :posts, :has_many => :comments
+  map.resources :comments
 
   # The priority is based upon order of creation: first created -> highest priority.
 
