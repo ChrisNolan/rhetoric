e725926aae8aee4d745726b059cbf9528e535638
Chris Nolan.ca <chrisnolan.ca+github@gmail.com>
2008-09-24 00:53:02 -0400

    Make the date calendar a bit more readable
    But it has a long way to go -- it's good for the search engines for the moment though

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,10 +1,30 @@
 module PostsHelper
   
+  # TODO there has to be some nice ruby block/iterator kind of thing to clean this up...
   def date_index_calendar
     html = '<ul>'
-    for r in Post.for_calendar
-      day = Date.parse "#{r.year}-#{r.month}-#{r.day}"
-      html << "<li>#{link_to day, date_index_path(day.to_s(:number))}: #{r.records}</li>"
+    daily_posts = Post.for_calendar
+    for year in daily_posts.first.year..daily_posts.last.year
+      daily_posts_per_year = daily_posts.select{|daily|daily.year == year}
+      unless daily_posts_per_year.empty?
+        html << '<ul class="years">'
+        html << "<li>#{year}</li>\n"
+        for month in 1..12
+          daily_posts_per_month = daily_posts_per_year.select{|daily|daily.month == month.to_s}
+          unless daily_posts_per_month.empty?
+            html << '<ul class="months">'
+            html << "<li>#{month}</li>\n"
+            html << '<ul class="days">'
+            for posted_day in daily_posts_per_month
+              day_of_month = Date.parse "#{posted_day.year}-#{posted_day.month}-#{posted_day.day}"
+              html << "<li>#{link_to day_of_month.to_s(:short), date_index_path(day_of_month.to_s(:number)), :title => pluralize(posted_day.records, 'post')}</li>\n"
+            end
+            html << '</ul>'
+            html << '</ul>'
+          end
+        end
+        html << '</ul>'
+      end
     end
     html << '</ul>'
   end
